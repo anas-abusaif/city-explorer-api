e7d50f745bd8cbecca9f70705c7fdf3cf28e242d
anas-abusaif <anasabusief@gmail.com>
2021-08-13 18:46:56 +0300

    update

--- controlers/Weather.controler.js
+++ controlers/Weather.controler.js
@@ -4,7 +4,7 @@ const Forcast = require('../models/Weather.model');
 
 
 const getWeather = (req, res) => {
-  let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=563e46f6cfd146f5bab7b9ab86aa9dcc&city=amman`
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=563e46f6cfd146f5bab7b9ab86aa9dcc&city=${req.query['searchQuery']}`
   let displayWeather = []
   axios.get(url).then(recived => {
    recived.data.data.forEach(dayy=>{
