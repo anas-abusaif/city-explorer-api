e3bd884ebc243725d7fd0ab8ba9c7240ce29cd73
anas-abusaif <anasabusief@gmail.com>
2021-08-13 14:22:20 +0300

    update

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ app.use(cors())
 require('dotenv').config();
 const PORT = process.env.PORT;
 const WeatherControler=require('./controlers/Weather.controler')
-
+const moviesControler=require('./controlers/Movies.controler')
 app.get('/',(req,res)=>{
   res.send('hello there')
 })
 
 app.get('/weather', WeatherControler);
 
+// app.get('/movies', moviesControler)
 
 app.listen(PORT, () => console.log('working'));
 
