6f5f0f2bb48f593177b1968f357d0bdf6efda01d
anas-abusaif <anasabusief@gmail.com>
2021-08-14 13:47:15 +0300

    update

--- controlers/Weather.controler.js
+++ controlers/Weather.controler.js
@@ -2,26 +2,26 @@
 const axios = require('axios')
 const Forcast = require('../models/Weather.model');
 const Cache = require('../helpers/ForcastCache')
-const forcastCache = new Cache();
+const forcastCache = new Cache([]);
 
 const getWeather = (req, res) => {
-  if (forcastCache.forcastData[req.query['searchQuery']].length) {
-    res.json({ data: forcastCache.forcastData[req.query['searchQuery']] })
+  if (forcastCache.forcastData.name === req.query['searchQuery']) {
+    res.send(forcastCache.forcastData.data)
   } else {
-
-
     let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=563e46f6cfd146f5bab7b9ab86aa9dcc&city=${req.query['searchQuery']}`
     let displayWeather = []
     axios.get(url).then(recived => {
       recived.data.data.forEach(day => {
         displayWeather.push(new Forcast(day))
       })
-      forcastCache.forcastData=displayWeather;
+      let weatherObj = {
+        name: req.query['searchQuery'],
+        data: displayWeather
+      }
+      forcastCache.forcastData = weatherObj;
       res.send(displayWeather);
     })
-
   }
-
 }
 
 module.exports = getWeather;
