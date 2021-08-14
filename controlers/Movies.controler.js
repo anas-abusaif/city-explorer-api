b40186e60b5b6af831fe6cf2920ffa071106b9fb
anas-abusaif <anasabusief@gmail.com>
2021-08-14 12:31:19 +0300

    update

--- controlers/Movies.controler.js
+++ controlers/Movies.controler.js
@@ -1,15 +1,22 @@
 'use strict';
 const axios = require('axios');
-const TopMovies=require('../models/Movie.model')
-
+const TopMovies = require('../models/Movie.model')
+const Cache = require('../helpers/MoviesCache')
+const moviesCache = new Cache();
 const getMovies = (req, res) => {
-  let url = `https://api.themoviedb.org/3/search/multi?api_key=d8a9bee92b0e8c76e9aa9ff88ce952c3&query=${req.query['searchQuery']}`
-  let movieData = []
-  axios.get(url).then(moviesData => {
-    moviesData.data.results.forEach(movie => {
-      movieData.push(new TopMovies(movie))
+
+  if (moviesCache.moviesBrief[req.query['searchQuery']].length) {
+    res.json({ data: moviesCache.moviesBrief[req.query['searchQuery']] })
+  } else {
+    let url = `https://api.themoviedb.org/3/search/multi?api_key=d8a9bee92b0e8c76e9aa9ff88ce952c3&query=${req.query['searchQuery']}`
+    let movieData = []
+    axios.get(url).then(moviesData => {
+      moviesData.data.results.forEach(movie => {
+        movieData.push(new TopMovies(movie))
+      })
+      moviesCache.moviesBrief = movieData;
+      res.send(movieData)
     })
-    res.send(movieData)
-  })
+  }
 }
 module.exports = getMovies;
--- controlers/Weather.controler.js
+++ controlers/Weather.controler.js
@@ -1,20 +1,27 @@
 'use strict';
 const axios = require('axios')
 const Forcast = require('../models/Weather.model');
-
+const Cache = require('../helpers/ForcastCache')
+const forcastCache = new Cache();
 
 const getWeather = (req, res) => {
-  let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=563e46f6cfd146f5bab7b9ab86aa9dcc&city=${req.query['searchQuery']}`
-  let displayWeather = []
-  axios.get(url).then(recived => {
-   recived.data.data.forEach(dayy=>{
-     displayWeather.push(new Forcast(dayy))
-   })
-    res.send(displayWeather);
-  })
+  if (forcastCache.forcastData[req.query['searchQuery']].length) {
+    res.json({ data: forcastCache.forcastData[req.query['searchQuery']] })
+  } else {
 
 
-  
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=563e46f6cfd146f5bab7b9ab86aa9dcc&city=${req.query['searchQuery']}`
+    let displayWeather = []
+    axios.get(url).then(recived => {
+      recived.data.data.forEach(day => {
+        displayWeather.push(new Forcast(day))
+      })
+      forcastCache.forcastData=displayWeather;
+      res.send(displayWeather);
+    })
+
+  }
+
 }
 
 module.exports = getWeather;
